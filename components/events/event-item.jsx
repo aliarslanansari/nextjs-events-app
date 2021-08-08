134d268501176e58ba102cfed281bc7b6116fc98
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-08 22:48:49 +0530

    added address, date icons

--- components/events/event-item.jsx
+++ components/events/event-item.jsx
@@ -1,6 +1,9 @@
-import Link from 'next/link'
 import Image from 'next/image'
 import classes from './event-item.module.css'
+import Button from '../ui/button'
+import DateIcon from './../icons/date-icon'
+import AddressIcon from './../icons/address-icon'
+import ArrowRightIcon from './../icons/arrow-right-icon'
 const EventItem = (props) => {
   const { id, title, location, date, image } = props
 
@@ -20,13 +23,20 @@ const EventItem = (props) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
           <div className={classes.actions}>
-            <Link href={exploreLink}>Explore Event</Link>
+            <Button link={exploreLink}>
+              <span>Explore Event</span>
+              <span className={classes.icon}>
+                <ArrowRightIcon />
+              </span>
+            </Button>
           </div>
         </div>
       </div>
