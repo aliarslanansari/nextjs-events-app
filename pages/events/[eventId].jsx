03479e68660c29e7e2f0019910d1618fca61c254
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-09 00:38:59 +0530

    added event details

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,8 +1,31 @@
+import { useRouter } from 'next/router'
+import EventSummary from '../../components/event-detail/event-summary'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventContent from '../../components/event-detail/event-content'
+import { getEventById } from '../../dummyData'
+
 const EventDetails = () => {
+  const router = useRouter()
+  const { eventId } = router.query
+
+  const event = getEventById(eventId)
+  if (!event) {
+    return <p>No Event Found</p>
+  }
+  // console.log(eventId)
   return (
-    <div>
-      <h1>Event Details</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   )
 }
 
