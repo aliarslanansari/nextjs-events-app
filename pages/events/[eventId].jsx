e34d353393e15a237cfdaf7ed3ddb3f6981614fa
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-11 23:52:16 +0530

    fetch eventDetails using gsp and gspath

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -2,14 +2,10 @@ import { useRouter } from 'next/router'
 import EventSummary from '../../components/event-detail/event-summary'
 import EventLogistics from '../../components/event-detail/event-logistics'
 import EventContent from '../../components/event-detail/event-content'
-import { getEventById } from '../../dummyData'
+import { getEventById, getAllEvents } from '../../dummyData'
 import ErrorAlert from '../../components/ui/error-alert'
 
-const EventDetails = () => {
-  const router = useRouter()
-  const { eventId } = router.query
-
-  const event = getEventById(eventId)
+const EventDetails = ({ event }) => {
   if (!event) {
     return (
       <ErrorAlert>
@@ -32,6 +28,21 @@ const EventDetails = () => {
       </EventContent>
     </>
   )
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId
+  const event = getEventById(eventId)
+  return {
+    props: { event },
+  }
+}
+export async function getStaticPaths() {
+  const events = getAllEvents()
+  return {
+    paths: events.map((event) => ({ params: { eventId: event.id } })),
+    fallback: false,
+  }
 }
 
 export default EventDetails
