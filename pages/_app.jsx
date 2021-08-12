8621f20bb9e225d2ee3e628c3dbca1a0eafee8a3
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-12 16:30:49 +0530

    wrapped app in context provider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,14 @@
 import Layout from '../components/layout/layout'
 import '../styles/globals.css'
-import Notification from './../components/ui/notification'
+import { NotificationContextProvider } from './../store/notification-context'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <Notification />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   )
 }
 
