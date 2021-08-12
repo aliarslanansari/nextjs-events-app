6ff347269e41d8cb7d3fa4efb0fd7cd9cf3d7c76
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-12 16:29:59 +0530

    added notification component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import Layout from '../components/layout/layout'
 import '../styles/globals.css'
+import Notification from './../components/ui/notification'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Notification />
     </Layout>
   )
 }
